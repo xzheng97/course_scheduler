fed0710792f1154d59d0e539af1a13fdc2d59a28
David Zheng <xzheng97@wisc.edu>
2020-09-17 02:05:57 -0400

    Updates

---
+++ precache-manifest.0c2a99f21bfb7a8e1dea0b6af24bfadb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80eb09b3a21d790a91f259ff9aa25b5",
+    "url": "/coursescheduler/index.html"
+  },
+  {
+    "revision": "341fc8572b3388076c0b",
+    "url": "/coursescheduler/static/css/2.8e1873ca.chunk.css"
+  },
+  {
+    "revision": "ab75d55045604604f75f",
+    "url": "/coursescheduler/static/css/main.8299f9c5.chunk.css"
+  },
+  {
+    "revision": "341fc8572b3388076c0b",
+    "url": "/coursescheduler/static/js/2.d135ae30.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/coursescheduler/static/js/2.d135ae30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab75d55045604604f75f",
+    "url": "/coursescheduler/static/js/main.afde0ead.chunk.js"
+  },
+  {
+    "revision": "40c4e925b1a179525ba3",
+    "url": "/coursescheduler/static/js/runtime-main.858fd0d1.js"
+  }
+]);
